f7c7342bfbd1f7bbcfcb3d33af83d1bd024343bb
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-14 13:45:24 +0100

    installed router dom

---
+++ src/Component/Sidebar.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Aside = styled.aside `
+
+`
+
+export default function Sidebar () {
+    return (
+        <Aside>
+            <nav>
+                
+            </nav>
+        </Aside>
+    )
+}
